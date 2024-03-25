81c598ec1d8401daa99fb09f4839924d0b38d33b
Burkan Akıncı <burkanakinci@gmail.com>
2024-03-26 02:50:29 +0300

    Added Color Contains

--- Assets/Scripts/Objects/PouringCup/PouringCupTargetLiquid.cs
+++ Assets/Scripts/Objects/PouringCup/PouringCupTargetLiquid.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Game.Object
@@ -5,10 +6,26 @@ namespace Game.Object
     public class PouringCupTargetLiquid : CustomBehaviour<PouringCupTarget>
     {
         [SerializeField] private MeshRenderer m_LiquidRenderer;
+        private Color m_TargetColor;
+        private const float m_PercentMultiply = 100.0f / 3.0f;
+        private float m_ContainsValue;
 
         public void SetLiquidColor(Color _color)
         {
-            m_LiquidRenderer.material.color = _color;
+            m_TargetColor = _color;
+            m_LiquidRenderer.material.color = m_TargetColor;
+        }
+
+        private float m_ColorTotalDist;
+        [Button]
+        public void ContainsPlayerColor(Color _color)
+        {
+            m_ColorTotalDist = 0.0f;
+            m_ColorTotalDist += Mathf.Abs(m_TargetColor.r - _color.r);
+            m_ColorTotalDist += Mathf.Abs(m_TargetColor.g - _color.g);
+            m_ColorTotalDist += Mathf.Abs(m_TargetColor.b - _color.b);
+            m_ColorTotalDist = 3.0f - m_ColorTotalDist;
+            m_ContainsValue = m_ColorTotalDist * m_PercentMultiply;
         }
     }
 }
