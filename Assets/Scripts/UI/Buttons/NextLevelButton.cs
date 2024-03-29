b90656f4f587f71e7ac8379d259e3d947ee2b6c2
Burkan Akıncı <burkanakinci@gmail.com>
2024-03-29 07:27:10 +0300

    Added Target MAtch Area Button Funcs

--- Assets/Scripts/UI/Buttons/NextLevelButton.cs
+++ Assets/Scripts/UI/Buttons/NextLevelButton.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Game.Manager;
+using Game.StateMachine;
+using Game.Utilities.Constants;
 using UnityEngine;
 
 namespace Game.UI
 {
     public class NextLevelButton : TargetColorMatchAreaButton
     {
-        [SerializeField] private bool m_IsRestart;
-        // protected override void OnClickAction()
-        // {
-        //     //if (!m_IsRestart)
-        //     //     GameManager.Instance.PlayerManager.Player.SetPlayerLevel(GameManager.Instance.PlayerManager.Player.PlayerLevel + 1);
-        //     // GameManager.Instance.ResetToMainMenu();
-        // }
+        private PlayerStateMachine m_PlayerStateMachine;
+        public override void Initialize(TargetColorMatchArea _cachedComponent)
+        {
+            base.Initialize(_cachedComponent);
+            m_PlayerStateMachine = GameManager.Instance.GetManager<PlayerManager>().Player.PlayerStateMachine;
+            OnClickTargetMatchAreaButton = NextLevel;
+        }
+        private void NextLevel()
+        {
+            m_PlayerStateMachine.ChangeStateTo(PlayerStates.WinState);
+        }
     }
 }
